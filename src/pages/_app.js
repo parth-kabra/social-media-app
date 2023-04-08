6de0830639657c284fbd84d9c04f57fa1c13fb60
Parth Santosh Kabra <kabraji001@gmail.com>
2023-04-08 20:08:54 +0530

    add tostify-js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import '@/styles/globals.css'
 import { Analytics } from '@vercel/analytics/react';
+import { ToastContainer } from 'react-toastify';
 
 export default function App({ Component, pageProps }) {
   return(
     <>
       <Component {...pageProps} />
+      <ToastContainer />
 		  <Analytics />
     </>
   )
