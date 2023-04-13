6a7b5ae8c54a181018df9deda8c972d3f9e44cd0
Parth Santosh Kabra <kabraji001@gmail.com>
2023-04-13 13:21:48 +0530

    update posts model

--- src/pages/api/posts.js
+++ src/pages/api/posts.js
@@ -3,28 +3,26 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default async function handler(req, res) {
-	if (req.method === "POST") {
+  	if (req.method === "POST") {
 		const { description, banner, likes, creator, pfp, key } = req.body
 
 		try {
 			const newPost = await prisma.post.create({
 				data: {
-					description: description,
-					banner: banner,
-					likes: likes,
-					creator: creator,
-					pfp: pfp,
-					key: key
+				description: description,
+				banner: banner,
+				likes: likes,
+				creator: creator,
+				pfp: pfp,
+				key: key,
 				}
 			})
 			res.status(201).json(newPost)
 		} catch (e) {
-			console.error(e)
 			res.status(500).json({ error: 'Unable to create post.' })
 		}
-	} else if (req.method === "PUT") {
-		// like the post stuff
-	} else {
+	} 
+	else {
 		const posts = await prisma.post.findMany()
 		res.status(200).json(posts)
 	}
